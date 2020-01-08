eba3e0291d06606d140d126261f25a13b67806fc
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-08 17:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => console.log(renderBooks(json)));
+    .then(json => renderBooks(json));
   }
 
 function renderBooks(json) {
